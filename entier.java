90d6e0c6f0eb91413e5ae506f15d95c3da5cf7cb
Schlim <schlim.romain@gmail.com>
2016-08-30 16:51:08 +0200

    Add files via upload

---
+++ entier.java
@@ -0,0 +1,28 @@
+package entier;
+
+public class entier {
+
+	public entier() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		/**
+		 *  1) ne compile pas
+			2) affiche 128
+			3) affiche -128
+			
+			byte i=127;
+			i++;
+			System.out.println(i);
+			
+			
+		 */
+		byte i=127;
+		i++;
+		System.out.println(i);
+	}
+}
+
+
